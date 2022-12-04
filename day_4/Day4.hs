2b0bfbf4b26b450369f127db9e65110f8d4ee9a3
Alexander Wood <alexljwood24@hotmail.co.uk>
2022-12-04 17:20:08 +0000

    Day 4

---
+++ day_4/Day4.hs
@@ -0,0 +1,35 @@
+import Control.Monad
+import Data.Bifunctor (Bifunctor (bimap), second)
+import Data.List
+import Data.Maybe (fromJust)
+
+data Range = Range Int Int
+
+contains :: Pair -> Bool
+contains (Range f1 l1, Range f2 l2) =
+    (f1 <= f2 && l1 >= l2) || (f2 <= f1 && l2 >= l1)
+
+overlaps :: Pair -> Bool
+overlaps (Range f1 l1, Range f2 l2) = l1 >= f2 && f1 <= l2
+
+type Pair = (Range, Range)
+
+splitOn :: Char -> String -> (String, String)
+splitOn c s = second tail $ splitAt (fromJust $ c `elemIndex` s) s
+
+mapBoth :: Bifunctor p => (c -> d) -> p c c -> p d d
+mapBoth = join bimap
+
+parsePair :: String -> Pair
+parsePair s =
+    let parts = splitOn ',' s
+     in mapBoth parseRange parts
+  where
+    parseRange s' = uncurry Range $ mapBoth read $ splitOn '-' s'
+
+main :: IO ()
+main = do
+    c <- lines <$> readFile "input.txt"
+    let pairs = parsePair <$> c
+    print (length (filter contains pairs))
+    print (length (filter overlaps pairs))
